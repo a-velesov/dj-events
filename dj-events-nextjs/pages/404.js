26c78cda0b449e2f3cf0fa3a5349c27f9b036cb7
Alexsandr <a.velesov@devkeeper.ru>
2021-05-24 13:33:11 +0300

    add 404 page & install icons

---
+++ dj-events-nextjs/pages/404.js
@@ -0,0 +1,20 @@
+import Layout from '../components/Layout';
+import styles from '../styles/404.module.css';
+import Link from 'next/link';
+import {FaExclamationTriangle} from 'react-icons/fa';
+
+const NotFoundPage = () => {
+    return (
+        <Layout title='Page not found'>
+            <div className={styles.error}>
+                <h1>
+                    <FaExclamationTriangle /> 404
+                </h1>
+                <h4>Sorry, there is nothing here</h4>
+                <Link href='/'>Go home</Link>
+            </div>
+        </Layout>
+    );
+};
+
+export default NotFoundPage;
