4c80cfe05ba90583485c62416ec076cac1e7336b
Alexsandr <a.velesov@devkeeper.ru>
2021-05-24 13:39:49 +0300

    add module aliases with jsconfig.json

--- dj-events-nextjs/pages/404.js
+++ dj-events-nextjs/pages/404.js
@@ -1,5 +1,5 @@
-import Layout from '../components/Layout';
-import styles from '../styles/404.module.css';
+import Layout from '@/components/Layout';
+import styles from '@/styles/404.module.css';
 import Link from 'next/link';
 import {FaExclamationTriangle} from 'react-icons/fa';
 
