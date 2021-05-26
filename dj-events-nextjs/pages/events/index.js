2a8d3b38af234c4e731bf86be6239f147341d158
Alexsandr <a.velesov@devkeeper.ru>
2021-05-26 11:28:55 +0300

    data fetching (use getStaticProps); add event items

--- dj-events-nextjs/pages/events/index.js
+++ dj-events-nextjs/pages/events/index.js
@@ -1,11 +1,32 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import {API_URL} from '@/config/index';
 
-const EventsPage = () => {
-  return (
-    <Layout>
-      <h1>My events</h1>
-    </Layout>
-  );
+const EventsPage = ({events}) => {
+    return (
+        <Layout>
+            <h1>Events</h1>
+            {
+                events.length === 0 && <h3>No events to show</h3>
+            }
+
+            {
+                events.map(evt => (
+                    <EventItem key={evt.id} evt={evt}/>
+                ))
+            }
+        </Layout>
+    );
 };
 
 export default EventsPage;
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: {events},
+        revalidate: 1,
+    };
+}
--- dj-events-nextjs/pages/index.js
+++ dj-events-nextjs/pages/index.js
@@ -1,11 +1,41 @@
 import Layout from '@/components/Layout';
+import {API_URL} from '@/config/index';
+import EventItem from '@/components/EventItem';
+import Link from 'next/link';
 
-const HomePage = () => {
-  return (
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
-  );
+const HomePage = ({events}) => {
+    return (
+        <Layout>
+            <h1>Upcoming Events</h1>
+            {
+                events.length === 0 && <h3>No events to show</h3>
+            }
+
+            {
+                events.map(evt => (
+                    <EventItem key={evt.id} evt={evt} />
+                ))
+            }
+
+            {
+                events.length > 0 && (
+                    <Link href='/events'>
+                        <a className="btn-secondary">View all Events</a>
+                    </Link>
+                )
+            }
+        </Layout>
+    );
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: {events: events.slice(0, 3)},
+        revalidate: 1,
+    };
+}
