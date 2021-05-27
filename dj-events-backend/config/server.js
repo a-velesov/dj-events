8bc78d0e64a8c243d08901897935f8dcb367fb36
Alexsandr <a.velesov@devkeeper.ru>
2021-05-27 16:03:40 +0300

    install strapi

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dc1543d7643856d809aaed0f5f84cb20'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f81d4f85-eab1-40a7-9851-a1e10a4994c9'
+};
