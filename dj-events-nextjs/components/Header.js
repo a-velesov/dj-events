b51c4f23ecdbc39899c37c7e0aed36a94dcde5b1
Alexsandr <a.velesov@devkeeper.ru>
2021-06-01 10:05:04 +0300

    add search

--- dj-events-nextjs/components/Header.js
+++ dj-events-nextjs/components/Header.js
@@ -1,25 +1,29 @@
 import styles from '@/styles/Header.module.css';
 import Link from 'next/link';
+import Search from '@/components/Search';
 
 const Header = () => {
-  return (
-    <header className={ styles.header }>
-      <div className={ styles.logo }>
-        <Link href='/'>
-          DJ Events
-        </Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/events'>
-              Events
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link href='/'>
+                    DJ Events
+                </Link>
+            </div>
+
+            <Search/>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/events'>
+                            Events
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
---
+++ dj-events-nextjs/pages/events/search.js
@@ -0,0 +1,47 @@
+import qs from 'qs';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import {API_URL} from '@/config/index';
+import {useRouter} from 'next/router';
+import Link from 'next/link';
+
+const SearchPage = ({events}) => {
+    const router = useRouter();
+
+    return (
+        <Layout title='Search result'>
+            <h1>Search result for '{router.query.term}'</h1>
+            <Link href='/'>Go to home</Link>
+            {
+                events.length === 0 && <h3>No events to show</h3>
+            }
+
+            {
+                events.map(evt => (
+                    <EventItem key={evt.id} evt={evt}/>
+                ))
+            }
+        </Layout>
+    );
+};
+
+export default SearchPage;
+
+export async function getServerSideProps({query: {term}}) {
+    const query = qs.stringify({
+        _where: {
+            _or: [
+                {name_contains: term},
+                {performers_contains: term},
+                {description_contains: term},
+                {venue_contains: term},
+            ]
+        }
+    });
+    const res = await fetch(`${API_URL}/events?${query}`);
+    const events = await res.json();
+
+    return {
+        props: {events},
+    };
+}
