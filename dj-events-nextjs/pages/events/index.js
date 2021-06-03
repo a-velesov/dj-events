73373587bf6a49a55612b1aa820857ba026ec187
Alexsandr <a.velesov@devkeeper.ru>
2021-06-03 11:13:38 +0300

    add pagination

--- dj-events-nextjs/pages/events/index.js
+++ dj-events-nextjs/pages/events/index.js
@@ -1,8 +1,9 @@
 import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
-import {API_URL} from '@/config/index';
+import {API_URL, PER_PAGE} from '@/config/index';
+import Pagination from '@/components/Pagination';
 
-const EventsPage = ({events}) => {
+const EventsPage = ({events, total, page}) => {
     return (
         <Layout>
             <h1>Events</h1>
@@ -15,17 +16,23 @@ const EventsPage = ({events}) => {
                     <EventItem key={evt.id} evt={evt}/>
                 ))
             }
+            <Pagination page={page} total={total} />
         </Layout>
     );
 };
 
 export default EventsPage;
 
-export async function getServerSideProps() {
-    const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-    const events = await res.json();
+export async function getServerSideProps({query: {page = 1}}) {
+    const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+    const totalRes = await fetch(`${API_URL}/events/count`);
+    const total = await totalRes.json();
+
+    const eventRes = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`);
+    const events = await eventRes.json();
 
     return {
-        props: {events},
+        props: {events, page: +page, total},
     };
 }
