c909066e3c9ec6fa5c5e1a6b507f006580a96b44
Alexsandr <a.velesov@devkeeper.ru>
2021-06-09 10:12:02 +0300

    [strapi] create is owner policy for events

--- dj-events-backend/api/events/controllers/events.js
+++ dj-events-backend/api/events/controllers/events.js
@@ -1,5 +1,5 @@
 'use strict';
-const {sanitizeEntity} = require('strapi-utils');
+const {sanitizeEntity, parseMultipartData} = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -7,13 +7,72 @@ const {sanitizeEntity} = require('strapi-utils');
  */
 
 module.exports = {
+  // https://strapi.io/documentation/developer-docs/latest/guides/is-owner.html#apply-the-author-by-default
+
+  // Create event with linked user
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const {data, files} = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(data, {files});
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, {model: strapi.models.events});
+  },
+
+  // Update user event
+  async update(ctx) {
+    const {id} = ctx.params;
+
+    let entity;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      'user.id': ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is('multipart')) {
+      const {data, files} = parseMultipartData(ctx);
+      entity = await strapi.services.events.update({id}, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.events.update({id}, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, {model: strapi.models.events});
+  },
+
+  // Delete a user event
+  async delete(ctx) {
+    const {id} = ctx.params;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      'user.id': ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+    const entity = await strapi.services.events.delete({id});
+    return sanitizeEntity(entity, {model: strapi.models.events});
+  },
+
   // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
 
     if (!user) {
       return ctx.badRequest(null, [
-        {messages: [{id: 'No authorization header was found'}]}
+        {messages: [{id: 'No userization header was found'}]}
       ]);
     }
 
